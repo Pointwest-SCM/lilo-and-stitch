831207be77034f9bd9bc4ecfae8cc68a7a4971e9
Arvin Linog <arvin.linog@pointwest.com.ph>
2018-05-03 13:55:30 +0800

    Added components for Brand Profile and Business Card

--- lns-poc/src/app/app.module.ts
+++ lns-poc/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { BrandProfileComponent } from './brand-profile/brand-profile.component';
+import { BusinessCardComponent } from './business-card/business-card.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrandProfileComponent,
+    BusinessCardComponent
   ],
   imports: [
     BrowserModule,
